89ff72a9bb19c6a6f7417833f3de7d5f6606f3e7
Tyler Krupicka <tylerkrupicka@gmail.com>
2019-03-23 12:50:53 -0700

    set up build and harness serve

---
+++ harness/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    harness
+    <json-view></json-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import JSONView from "../src/JSONView.vue";
+
+@Component({
+  components: { "json-view": JSONView }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+}
+</style>
---
+++ src/JSONView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">JSON View</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+}
+</style>
